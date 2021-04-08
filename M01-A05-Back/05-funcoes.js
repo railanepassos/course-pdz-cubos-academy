e25b5407061fc451181d5236d6b6624fdeb995d6
Railane Passos <railanepassos@gmail.com>
2021-04-08 18:41:43 -0300

    Andamento 07/04/2021

---
+++ M01-A05-Back/05-funcoes.js
@@ -0,0 +1,11 @@
+/*
+    Funções é um bloco de código que executa uma tarefa
+        especifica ou calcula um valor
+
+    Sintaxe
+    function nomeDaFuncao (parametros) {
+        // Seu código aqui
+    }
+
+    Os parametros da função são dados que vamos passar para essa função
+*/
